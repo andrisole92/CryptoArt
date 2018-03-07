9ee5e908f1493d77623dca8983bbf2a841bc8e02
andrisole92 <andrisole92@interstruct.com>
2018-03-07 14:40:29 +0000

    Server

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const shell = require('shelljs');
-
-if (!shell.which('git')) {
-    shell.echo('Sorry, this script requires git');
-    shell.exit(1);
-} else {
-    shell.echo('good');
-
-}
 
 
 app.use(express.static(path.join(__dirname, 'build')));
