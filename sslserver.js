8bc335599a35bdca5843c2541f7bd5ee9beccd0d
andrisole92 <andrisole92@interstruct.com>
2018-03-13 17:39:11 +0000

    ssl

--- sslserver.js
+++ sslserver.js
@@ -7,8 +7,8 @@ const https = require('https');
 // const privateKey  = fs.readFileSync('./sslcert/privkey.pem', 'utf8');
 // const certificate = fs.readFileSync('./sslcert/fullchain.pem', 'utf8');
 const options = {
-    key: fs.readFileSync('./sslcert/privkey.perm'),
-    cert: fs.readFileSync('./ssslcert/fullchain.perm')
+    key: fs.readFileSync('./sslcert/privkey.pem'),
+    cert: fs.readFileSync('./ssslcert/fullchain.pem')
 };
 // const credentials = {key: privateKey, cert: certificate};
 const express = require('express');
