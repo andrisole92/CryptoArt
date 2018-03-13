ff18c6768d80fa3031bf22209070d29a2b7a449a
andrisole92 <andrisole92@interstruct.com>
2018-03-13 17:38:29 +0000

    ssl

--- sslserver.js
+++ sslserver.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const path = require('path');
 const fs = require('fs');
+const path = require('path')
+
 const http = require('http');
-
 const https = require('https');
-
+//
+// const privateKey  = fs.readFileSync('./sslcert/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('./sslcert/fullchain.pem', 'utf8');
+const options = {
+    key: fs.readFileSync('./sslcert/privkey.perm'),
+    cert: fs.readFileSync('./ssslcert/fullchain.perm')
+};
+// const credentials = {key: privateKey, cert: certificate};
+const express = require('express');
 const app = express();
 
-let certOpts = {
-    key: fs.readFileSync('./sslcert/privkey.pem'),
-    cert: fs.readFileSync('./sslcert/fullchain.pem')
-};
-https.createServer({}, app).listen(9090);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+
+httpServer.listen(8082);
+httpsServer.listen(8081);
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(require('helmet')());
 
+
+//Force https
+app.use(function(req, res, next) {
+    if(!req.secure) {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+});
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-
-let httpApp = express();
-let httpServer = http.createServer(httpApp);
-
-httpApp.use(express.static(path.join(__dirname, 'build')));
-
-// set up a route to redirect http to https
-httpApp.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-httpServer.listen(80);
-
-// module.exports = app;
